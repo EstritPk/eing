f2b67d7d9b5fc94ac7eff8f98656f87111455ae5
EstritPk <street.rojas69@gmail.com>
2022-05-16 03:33:24 -0400

    a

---
+++ ConsoleAppWithMaksi/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppWithMaksi
+{
+    internal class Class1
+    {
+        public void Menu()
+        {
+            Console.WriteLine("************************************");
+            Console.WriteLine("*************MENU DE OPCIONES*******");
+            Console.WriteLine("Buena po Maksi");
+            Console.WriteLine("1.- Saludar a Maksi");
+            Console.WriteLine("2.- Saludar a Zanno");
+            Console.WriteLine("3.- Saludar a Maty");
+            Console.WriteLine("4.- Saludar a Estrit");
+            Console.WriteLine("0.- Astalaproksima");
+            Console.WriteLine("************************************");
+        }
+
+     }
---
+++ ConsoleAppWithMaksi/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+public void Menu() { 
+Console.WriteLine("************************************");
+Console.WriteLine("*************MENU DE OPCIONES*******");
+Console.WriteLine("Buena po Maksi");
+Console.WriteLine("1.- Saludar a Maksi");
+Console.WriteLine("2.- Saludar a Zanno");
+Console.WriteLine("3.- Saludar a Maty");
+Console.WriteLine("4.- Saludar a Estrit");
+Console.WriteLine("0.- Astalaproksima");
+Console.WriteLine("************************************");
+    )
